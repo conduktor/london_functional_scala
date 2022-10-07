f26277e6b06da6b0249eb1999bb4906345672bce
Matthieu Baechler <matthieu@conduktor.io>
2022-10-07 10:42:22 +0200

    Retrieve only topic names and display "loading" for all fields we don't have yet

--- src/Table.elm
+++ src/Table.elm
@@ -1,15 +1,37 @@
-module Table exposing (tableHtml, TopicInfo)
+module Table exposing (tableHtml, TopicInfo, TopicName, mkTopicName, topicNamesToTopicInfos, mkTopicInfo)
 
 import Html exposing (table, text, td, tr, th, Html, Attribute)
 import Html.Attributes exposing (style)
-type alias TopicInfo = {name: String, sizeInByte: String, partition: String, recordCount: String, spread: String, replicationFactor: String}
+
+type Datapoint = Loading | Loaded String
+type alias TopicInfo = {name: Datapoint, sizeInByte: Datapoint, partition: Datapoint, recordCount: Datapoint, spread: Datapoint, replicationFactor: Datapoint}
+
+initialTopicInfo = {name = Loading, sizeInByte = Loading, partition = Loading, recordCount = Loading, spread = Loading, replicationFactor = Loading}
+mkTopicInfo name
+            sizeInByte
+            partition
+            recordCount
+            spread
+            replicationFactor = {name = Loaded name,
+                                 sizeInByte = Loaded sizeInByte,
+                                 partition = Loaded partition,
+                                 recordCount = Loaded recordCount,
+                                 spread = Loaded spread,
+                                 replicationFactor = Loaded replicationFactor}
+
+type TopicName = TopicName String
+
+mkTopicName = TopicName
 
 headers = ["topic", "size in bytes", "partitions", "records count", "spread", "replication factor"]
 
 borderStyle = [style "border" "1px solid black", style "border-collapse" "collapse"]
 
-datapointToCell: String -> Html msg
-datapointToCell s = td borderStyle [text s]
+datapointToCell: Datapoint -> Html msg
+datapointToCell datapoint = case datapoint of
+    Loaded s -> td borderStyle [text s]
+    Loading -> td borderStyle [text "loading"]
+
 headerToCell s = th borderStyle [text s]
 
 arrayToTr f s = List.map f s |> tr borderStyle
@@ -24,3 +46,8 @@ tableHtml topics = table borderStyle <| List.append
         [headerLine]
         <| List.map topicToHtml topics
 
+topicNamesToTopicInfos: List TopicName -> List TopicInfo
+topicNamesToTopicInfos names =
+    List.map (\topicName ->
+     let topicInfo = initialTopicInfo in
+     let (TopicName name) = topicName in { topicInfo | name = Loaded name}) names
